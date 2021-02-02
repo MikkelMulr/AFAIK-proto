f10b854958ef3b5dab73d61e1a29a3d799301bbe
Mike.Mueller <michael17mueller@gmail.com>
2021-02-01 22:25:46 -0500

    initial

---
+++ src/pages/home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function home() {
+  return (
+    <div className="Home">
+      <section className="left"></section>
+      <section className="right"></section>
+    </div>
+  )
+}
